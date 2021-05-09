3910df6d7422adabc2e686f80d078765044c62d4
Lauren Jenkinson <lauren@kat.net.nz>
2021-05-09 19:22:35 +1200

    auth/login: Add working login

--- lib/Controllers/AuthLoginController.php
+++ lib/Controllers/AuthLoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace MagentaServer\Controllers;
 
+use MagentaServer\Models\User;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,8 +11,16 @@ class AuthLoginController extends Controller {
 		if ($this->session->isLoggedIn()) {
 			return $response->withHeader('Location', $this->urlFor('index'))->withStatus(302);
 		}
+		
+		$queryParams = $request->getQueryParams();
+		$message = null;
+		if (array_key_exists('from', $queryParams) && $queryParams['from'] == 'logout') {
+			$message = \L('auth_logout_success');
+		}
 
-		return $this->renderView($request, $response, "auth/login.html");
+		return $this->renderView($request, $response, "auth/login.html", [
+			'message' => $message,
+		]);
 	}
 
 	public function requestPOST(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
@@ -19,9 +28,36 @@ class AuthLoginController extends Controller {
 			return $response->withHeader('Location', $this->urlFor('index'))->withStatus(302);
 		}
 
-		$this->csrf->check('csrf', $request->getParsedBody()['_csrf'], null, true);
-		return $this->renderView($request, $response, "auth/login.html", [
-			'message' => 'Not implemented!',
-		]);
+		$requestBody = $request->getParsedBody();
+		$this->csrf->check('csrf', $requestBody['_csrf'], null, true);
+		
+		// Get parameters
+		$email = empty($email = trim($requestBody['email'] ?? '')) ? null : $email;
+		$password = empty($password = trim($requestBody['password'] ?? '')) ? null : $password;
+		if ($email === null || $password === null) {
+			return $this->renderView($request, $response, "auth/login.html", [
+				'message' => \L('auth_login_error_invalid_creds'),
+			]);
+		}
+		
+		// Get user object
+		$user = User::where('email', '=', $email)->first();
+		if ($user === null) {
+			return $this->renderView($request, $response, "auth/login.html", [
+				'message' => \L('auth_login_error_invalid_creds'),
+			]);
+		}
+
+		// Check password
+		if (password_verify($password, $user->password_hash) === false) {
+			return $this->renderView($request, $response, "auth/login.html", [
+				'message' => \L('auth_login_error_invalid_creds'),
+			]);
+		}
+		
+		// If we get here, login is successful
+		$this->session->session_data['userid'] = $user->id;
+		$this->session->ensureCreate()->update();
+		return $response->withHeader('Location', $this->urlFor('index'))->withStatus(302);
 	}
 }
