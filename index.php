2960cfbb20cff972b198198fcae86af0554d1a9a
Lauren Jenkinson <lauren@kat.net.nz>
2021-05-10 04:27:47 +1200

    app: Support out-of-tree (by symlink) execution of `index.php`

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/vendor/autoload.php';
+require_once(dirname(($p = realpath(__FILE__)) === false ? __FILE__ : $p) . '/vendor/autoload.php');
 
 \MagentaServer\Application::createApp()->run();
