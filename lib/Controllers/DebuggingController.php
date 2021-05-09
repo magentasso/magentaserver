aedd3c7b59edea6501a33bff200be6582a70ccf0
Lauren Jenkinson <lauren@kat.net.nz>
2021-05-09 19:23:20 +1200

    debug: Add ability to arbitrarily create users

--- lib/Controllers/DebuggingController.php
+++ lib/Controllers/DebuggingController.php
@@ -1,6 +1,7 @@
 <?php
 namespace MagentaServer\Controllers;
 
+use MagentaServer\Models\User;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,7 +12,8 @@ class DebuggingController extends Controller {
 	}
 
 	public function requestPOST(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-		$action = trim($request->getParsedBody()['action']);
+		$requestBody = $request->getParsedBody();
+		$action = trim($requestBody['action']);
 		if ($action === 'destroySession') {
 			$old_session_id = $this->session->session_id;
 			$this->session->destroy();
@@ -26,6 +28,42 @@ class DebuggingController extends Controller {
 
 			return $this->renderView($request, $response, "debug/message.html", [
 				'message' => \L('debug_view_session_populate_success', [$this->session->session_id ?? 'null']),
+			]);
+
+		} elseif ($action === 'createUser') {
+			$this->csrf->check('csrf', $requestBody['_csrf'], null, true);
+
+			// Get parameters
+			$email = empty($email = trim($requestBody['email'] ?? '')) ? null : $email;
+			$perms = empty($perms = trim($requestBody['perms'] ?? '')) ? '' : $perms;
+			if ($email === null) {
+				return $this->renderView($request, $response, "debug/message.html", [
+					'message' => \L('debug_view_createuser_error_no_email'),
+				]);
+			}
+
+			// Hash password
+			$password = 'password';
+			$password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+			$user = User::create([
+				'email' => $email,
+				'password_hash' => $password_hash,
+				'name_first' => 'Test',
+				'name_last' => 'User',
+				'permissions' => $perms,
+			]);
+
+			if ($user === null) {
+				return $this->renderView($request, $response, "debug/message.html", [
+					'message' => \L('debug_view_createuser_error_create_failed'),
+				]);
+			}
+
+			$user->save();
+
+			return $this->renderView($request, $response, "debug/message.html", [
+				'message' => \L('debug_view_createuser_success', [$user->id, $user->email, $password]),
 			]);
 		}
 
